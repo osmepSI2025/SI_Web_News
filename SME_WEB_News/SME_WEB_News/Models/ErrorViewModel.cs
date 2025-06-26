fea47e07a07eccf7ca5b16107eb684e9bcb4b48b
osmepSI2025 <osmep.si2025@gmail.com>
2025-06-26 16:32:59 +0700

    delete

--- SME_WEB_News/SME_WEB_News/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SME_WEB_News.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SME_WEB_News/SME_WEB_News/Models/LogInModels.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SME_WEB_News.Models
-{
-    public class LogInModels
-    {
-       public string samlResponse { get; set; }
-    }
-}
